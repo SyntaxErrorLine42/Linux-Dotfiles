b39d3a9eadabb49e97e6033e0adbc859c55bedb1
Luka <lukavolarevic11@gmail.com>
2025-08-15 14:54:51 +0200

    Added select all for nvim

--- nvim-local/lua/chadrc.lua
+++ nvim-local/lua/chadrc.lua
@@ -1,17 +1,17 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "dark_horizon",
+  theme = "dark_horizon",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
 }
 
 -- M.nvdash = { load_on_startup = true }
@@ -20,7 +20,8 @@ M.base46 = {
 --          lazyload = false
 --      }
 --}
-vim.opt.clipboard = ""  -- disables automatic system clipboard
-vim.keymap.set({'n', 'v'}, 'y', '"+y', { noremap = true, silent = true })
-vim.keymap.set('n', 'Y', '"+Y', { noremap = true, silent = true })
+vim.opt.clipboard = "" -- disables automatic system clipboard
+vim.keymap.set({ "n", "v" }, "y", '"+y', { noremap = true, silent = true })
+vim.keymap.set("n", "Y", '"+Y', { noremap = true, silent = true })
+vim.keymap.set({ "n", "i", "v" }, "<C-a>", "<Esc>ggVG$", { noremap = true, silent = true, desc = "Select all" })
 return M
