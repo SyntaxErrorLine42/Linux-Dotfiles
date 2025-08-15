0fa2a2370df163c076229cab708541d91c9a4cb9
Luka <lukavolarevic11@gmail.com>
2025-08-15 14:08:39 +0200

    Changed nvim submodule to nvim-local

---
+++ nvim-local/lua/chadrc.lua
@@ -0,0 +1,26 @@
+-- This file needs to have same structure as nvconfig.lua 
+-- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
+-- Please read that file to know all available options :( 
+
+---@type ChadrcConfig
+local M = {}
+
+M.base46 = {
+	theme = "dark_horizon",
+
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
+}
+
+-- M.nvdash = { load_on_startup = true }
+-- M.ui = {
+--       tabufline = {
+--          lazyload = false
+--      }
+--}
+vim.opt.clipboard = ""  -- disables automatic system clipboard
+vim.keymap.set({'n', 'v'}, 'y', '"+y', { noremap = true, silent = true })
+vim.keymap.set('n', 'Y', '"+Y', { noremap = true, silent = true })
+return M
